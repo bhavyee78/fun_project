7663e652d1064a618de0e53765397f730b8c2a94
kshitiz-bakshi <65666089+kshitiz-bakshi@users.noreply.github.com>
2020-08-02 12:27:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let para= document.getElementById('js');
+var audio = new Audio('abc1.mp3');
 let container=document.getElementById('container')
 let main=document.getElementById("main")
 js.addEventListener('click', function run(){
@@ -7,7 +8,7 @@ js.style.display='none';
 container.style.display='block';
 main.style.backgroundColor = "white";
 
-var audio = new Audio('abc1.mp3');
+
 audio.play();
 
 
