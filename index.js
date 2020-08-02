65e84a62d6b7ff7462111491986a295478725ad7
kshitiz-bakshi <65666089+kshitiz-bakshi@users.noreply.github.com>
2020-08-02 12:29:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var audio = new Audio('abc1.mp3');
 let container=document.getElementById('container')
 let main=document.getElementById("main")
 js.addEventListener('click', function run(){
+audio.play();
 console.log("Mouse inside");
 js.style.display='none';
 container.style.display='block';
 main.style.backgroundColor = "white";
 
 
-audio.play();
+
 
 
 });
